9883cac3dbf5af6ecf09a413d5d410871767d9d3
Ryota Morimoto <morimoto@isi.imi.i.u-tokyo.ac.jp>
2019-10-12 14:37:54 +0900

    file upload

--- slack_notification.py
+++ slack_notification.py
@@ -10,3 +10,27 @@ def slack_notification(text, webhook_url, username=None, **kwargs):
         data.update(kwargs)
 
     requests.post(webhook_url, data = json.dumps(data))
+
+
+def slack_file_upload(filepath, channel, text, token=None, client=None):
+    if token == None and client == None:
+        raise ValueError("Both token and client are None.")
+
+    import slack
+
+    if client != None:
+        client = client
+    else:
+        # make slack client
+        client = slack.WebClient(token=token)
+
+    if os.path.isfile(channel):
+        # if channel is file, read file
+        with open(channel, 'r') as f:
+            c = f.read().rstrip('\n')
+    else:
+        # else, channel is raw channel
+        c = channel
+
+    # file upload
+    client.files_upload(channels=c, file=filepath, initial_comment=text)
