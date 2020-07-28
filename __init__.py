4e58eeb45e58efc2f9b7e15804813b9369102ade
Ryota Morimoto <morimoto@isi.imi.i.u-tokyo.ac.jp>
2020-07-28 14:53:52 +0900

    mod and add __all__

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
-from my_util.get_vacant_gpu import *
-from my_util.slack_notification import *
+from my_util.get_vacant_gpu import get_vacant_gpu
+from my_util.slack_notification import slack_notification, slack_file_upload
+# from my_util.pdf_image_extractor import pdf_image_extractor
+# from my_util.pdf2png import main as pdf2png
+from my_util.dict_arg_parse import StoreDictKeyPair
+
+__all__ = [
+    # get_vacant_gpu
+    "get_vacant_gpu",
+    # slack_notification
+    "slack_notification",
+    "slack_file_upload",
+    # # pdf-image-extractor
+    # "pdf_image_extractor",
+    # # pdf2png
+    # "pdf2png",
+    # dict_arg_parse
+    "StoreDictKeyPair",
+]
