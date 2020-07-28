7c590ee263d65c4e43a3283af471ebefa77be86d
Ryota Morimoto <morimoto@isi.imi.i.u-tokyo.ac.jp>
2020-07-28 14:53:34 +0900

    add file

---
+++ dict_arg_parse.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+class StoreDictKeyPair(argparse.Action):
+    """
+    For argparse
+    parser.add_argument("--key_pairs", dest="my_dict", action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL")
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        self._nargs = nargs
+        super().__init__(option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        param_dict = getattr(namespace, self.dest, [])
+        if param_dict is None:
+            param_dict = {}
+        for kv in values:
+            k, v = kv.split("=")
+            param_dict[k] = v
+        setattr(namespace, self.dest, param_dict)
