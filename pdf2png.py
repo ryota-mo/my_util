90b3ff40d1e46835cb9a473613866aab7c6654dd
Ryota Morimoto <morimoto@isi.imi.i.u-tokyo.ac.jp>
2022-03-13 01:17:41 +0900

    add feature

--- pdf2png.py
+++ pdf2png.py
@@ -7,25 +7,27 @@ pip install pdf2image
 """
 
 
-def main(pdfpath, dpi=300):
+def main(pdfpath, dpi=300, is_jpg=False):
+    ext = "jpg" if is_jpg else "png"
     pdfpath = Path(pdfpath)
     image = convert_from_path(pdfpath, dpi)
     if len(image) == 1:
-        pngpath = pdfpath.parent.resolve() / (pdfpath.stem + '.png')
+        pngpath = pdfpath.parent.resolve() / (pdfpath.stem + f'.{ext}')
         if pngpath.is_file():
             raise FileExistsError(str(pngpath))
-        image[0].save(pngpath, 'png')
+        image[0].save(pngpath)
     else:
         for i in range(len(image)):
-            pngpath = pdfpath.parent.resolve() / (pdfpath.stem + f'_{i}.png')
+            pngpath = pdfpath.parent.resolve() / (pdfpath.stem + f'_{i}.{ext}')
             if pngpath.is_file():
                 raise FileExistsError(str(pngpath))
-            image[i].save(pngpath, 'png')
+            image[i].save(pngpath)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('pdfpath')
-    parser.add_argument('--dpi', type=int, default=300, help="defualt: 300")
+    parser.add_argument('--dpi', type=int, default=300, help="default: 300")
+    parser.add_argument('-j', '--jpg', action="store_true", help="output jpg")
     args = parser.parse_args()
-    main(args.pdfpath, args.dpi)
+    main(args.pdfpath, args.dpi, is_jpg=args.jpg)
